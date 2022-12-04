f21adee4483c59fe40ef9fcbc4c89e54d68cb21a
Jake <Jake@MATEBOOK>
2022-12-04 16:33:56 +0000

    #2 made camera functions inline

--- main/camera.hpp
+++ main/camera.hpp
@@ -18,39 +18,39 @@ typedef struct _cameraControl
 		} cameraControl;
 
 // Vector to move camera to the right by 1 unit
-Vec3f cam_right(const cameraControl* camera)
+inline Vec3f cam_right(const cameraControl* camera)
 {
 	return normalize(cross(camera->direction, camera->up));
 	
 }
 
 // Vector to move camera to the left by 1 unit
-Vec3f cam_left(const cameraControl* camera)
+inline Vec3f cam_left(const cameraControl* camera)
 {
 	return -normalize(cross(camera->direction, camera->up));
 	
 }
 
 // Vector to move camera up by 1 unit
-Vec3f cam_up(const cameraControl* camera)
+inline Vec3f cam_up(const cameraControl* camera)
 {
 	return normalize(camera->up);
 }
 
 // Vector to move camera down by 1 unit
-Vec3f cam_down(const cameraControl* camera)
+inline Vec3f cam_down(const cameraControl* camera)
 {
 	return -normalize(camera->up);
 }
 
 // Vector to move camera forwards by 1 unit
-Vec3f cam_forwards(const cameraControl* camera)
+inline Vec3f cam_forwards(const cameraControl* camera)
 {
 	return normalize(camera->direction);
 }
 
 // Vector to move camera backwards by 1 unit
-Vec3f cam_backwards(const cameraControl* camera)
+inline Vec3f cam_backwards(const cameraControl* camera)
 {
 	return -normalize(camera->direction);
 }
